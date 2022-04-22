aa01694657daa6a153dfda9064c9d5a16a522e91
Ana Cristina <ana.cris372@gmail.com>
2022-04-21 21:26:42 -0300

    conditionally add class to mark tasks as done

--- Script.js
+++ Script.js
@@ -1,29 +1,25 @@
 const novaTarefa = [];
 
+
 function tarefa(nome){
     return{
         nome,
         done: false
+       
     }
 };
-
+function grita (i){
+    novaTarefa[i].done = true
+    renderList();
+  
+};                
+function  renderList(){
+    document.querySelector("ul").innerHTML= novaTarefa.map((novaTarefa,i) => `<li class="${ novaTarefa.done ? 'done' : 'todo'}">${novaTarefa.nome}</li><button onclick="grita(${i})">Done</button>`)
+    
+};
 function clique(){
     const minhaTarefa = document.querySelector("#meuInput");
     const objeto = tarefa(minhaTarefa.value);
     const adicionandoElemento = novaTarefa.push(objeto);
-     document.querySelector("ul").innerHTML= novaTarefa.map(novaTarefa => `<li>${novaTarefa.nome}</li>`)
-
+    renderList()
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
